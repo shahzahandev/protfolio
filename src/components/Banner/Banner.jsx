8113a64fa26b87fb1814649fa0dff69708c64835
shahzahandev <shahzahansiraj516@gmail.com>
2026-01-12 00:02:36 +0600

    banner image customazie

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -19,13 +19,13 @@ const Banner = () => {
       <div className="bg-gray-800 font-primary md:pt-[200px] pt-[100px] md:pb-[50px] pb-[50px] overflow-y-hidden tracking-[1px] ">
         <Container>
           <div className="md:flex md:justify-between items-center md:px-0 px-5">
-            <div className="flex items-center md:hidden md:w-[80%] relative drop-shadow-[0_0px_15px_rgb(12_241_153_/_.5)] mb-5">
-              <img src={banner1} alt="" className="rounded-[50%]" />
+            <div className="flex items-center md:hidden w-[100%] relative drop-shadow-[0_0px_15px_rgb(12_241_153_/_.15)] mb-5">
+              <img src={banner1} alt="" className="rounded-[25px]" />
             </div>
-            <div className="md:w-[70%]">
+            <div className="md:w-[70%] md:text-start text-center md:mt-0 mt-8">
               <h4 className="md:text-[24px] text-[#c9c4c4]">Hello, It's Me</h4>
-              <h1 className="md:text-6xl text-[34px] md:w-auto w-[400px] font-semibold text-[#0cf199] md:mt-[15px]">Shahzahan Siraj</h1>
-              <h3 className="md:text-3xl text-[#c9c4c4] md:mt-[20px] md:w-auto w-[400px]">And I'm a <span className="md:text-4xl text-[22px] text-[#eafaf8]">Frontend Developer</span></h3>
+              <h1 className="md:text-6xl text-[34px] md:w-auto w-[300px] mx-auto font-semibold text-[#0cf199] md:mt-[15px]">Shahzahan Siraj</h1>
+              <h3 className="md:text-3xl text-[#c9c4c4] md:mt-[20px] md:w-auto w-[330px] mx-auto ">And I'm a <span className="md:text-4xl text-[22px] text-[#eafaf8]">Frontend Developer</span></h3>
               <p className="md:text-[16px] text-[14px] text-[#c9c4c4] md:w-[500px] w-aut0 md:mt-[20px] mt-2 leading-7">I specialize in creating pixel-perfect, high-performance Website & web applications using modern technologies like HTML, CSS, Bootstrap, Tailwind, React & Javascript.</p>
               <div className="flex md:flex-row flex-col items-center md:mt-[25px] mt-6 md:space-x-10 md:space-y-0 space-y-5">
                 <button className="flex items-center md:px-[32px] px-[100px] md:py-[13px] py-[10px] border-2 border-[#0cf199] rounded-[50px] bg-transparent text-[14px] font-semibold text-[rgb(12,241,153)]  transition-all duration-300 ease-in hover:shadow-[0px_0px_10px_rgba(12,241,153,1)] md:w-auto w-[370px] cursor-pointer">DOWNLOAD CV
@@ -35,10 +35,9 @@ const Banner = () => {
                     <button className="flex items-center md:px-[32px] px-[100px] md:py-[13px] py-[10px] border-2 border-[#0cf199] rounded-[50px] bg-transparent text-[14px] font-semibold text-[rgb(12,241,153)]  transition-all duration-300 ease-in hover:shadow-[0px_0px_10px_rgba(12,241,153,1)] md:w-auto w-[370px] cursor-pointer">View project
                   <FaArrowRight  className="md:ml-2 ml-5 font-bold text-[18px]" />
                 </button>
-             
                 </NavLink>
               </div>
-              <div className="font-main text-white  md:mt-10 mt-5">
+              <div className="font-main text-white  md:mt-10 mt-5 md:block hidden">
                   <ul className="flex md:space-x-[110px] space-x-10 text-[#0cf199]">
                     <li className="border-2 rounded-[50%] p-2 text-[24px] transition-all duration-100 ease-in hover:shadow-[0px_0px_10px_rgba(12,241,153,1)]">
                     <a href="https://www.linkedin.com/in/shahzahan-siraj-7b5ba7304/"><FaLinkedinIn className='' /></a>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,10 @@ import { MdOutlineMarkEmailUnread } from "react-icons/md";
 import { LuPhoneCall } from "react-icons/lu";
 import FooterLast from '../FooterLast/FooterLast';
 import { FaLocationDot } from "react-icons/fa6";
-
+import { FaFacebookF} from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
 const Footer = () => {
     return (
         <>
@@ -48,8 +51,10 @@ const Footer = () => {
                         </div>
                         <div className='md:w-[60%] w-[100%] md:px-5'>
                             <div className='md:py-10 py-5 md:px-5 px-0'>
-                                <h2 className='md:text-2xl text-[20px] font-bold text-[#0cf199] md:mb-5 mb-3 '>Let's work together</h2>
-                                <p className="md:text-[16px] text-[12px] text-[#c9c4c4] md:leading-7 leading-6 mb-5 md:w-auto w-[300px]">Whether you have a specific project in mind or just want to chat about the latest tech, I'm always open to new conversations.</p>
+                                <div className='flex flex-col md:items-start items-center'>
+                                       <h2 className='md:text-2xl text-[20px] font-bold text-[#0cf199] md:mb-5 mb-3 '>Let's work together</h2>
+                                <p className="md:text-[16px] text-[12px] text-[#c9c4c4] md:leading-7 leading-6 mb-5 md:w-auto w-[350px] mx-auto md:text-start text-center">Whether you have a specific project in mind or just want to chat about the latest tech, I'm always open to new conversations.</p>
+                                </div>
                                 <div className='flex flex-col space-y-3'>
                                     <input type="text" placeholder='Full name' className='text-white border border-gray-300 rounded-[15px] px-5 py-4  cursor-pointer md:text-[18px] text-[12px] outline-none  focus:border-[#0cf199] focus:ring-2 focus:ring-[#0cf199]/40' />
                                     <input type="text" placeholder='Email address ' className='text-white border border-gray-300 rounded-[15px] px-5 py-4  cursor-pointer md:text-[18px] text-[12px] outline-none focus:border-[#0cf199] focus:ring-2 focus:ring-[#0cf199]/40 ' />
@@ -66,10 +71,27 @@ const Footer = () => {
                                         ></textarea>
                                     </div>
                                     <div className='flex justify-center text-center w-full'>
-                                        <button className="text-black py-[13px] border-2 w-full border-[#0cf199] rounded-[50px] md:text-[18px]
+                                        <button className="text-black py-[13px] border-2 w-full border-[#0cf199] rounded-[10px] md:text-[18px]
                                         text-[14px] font-extrabold bg-[#0cf199] transition-all duration-300 ease-in hover:text-white hover:bg-transparent text-center cursor-pointer hover:shadow-[0px_0px_10px_rgba(12,241,153,1)]">Send a Message</button>
                                     </div>
                                 </div>
+                                   <div className="font-main text-white md:mt-10 mt-6 md:hidden block">
+                                                  <ul className="flex justify-center md:space-x-[110px] space-x-12 text-[#0cf199]">
+                                                    <li className="border-2 rounded-[50%] p-2 text-[24px] transition-all duration-100 ease-in hover:shadow-[0px_0px_10px_rgba(12,241,153,1)]">
+                                                    <a href="https://www.linkedin.com/in/shahzahan-siraj-7b5ba7304/"><FaLinkedinIn className='' /></a>
+                                                  </li>
+                                                  <li className="border-2 rounded-[50%] p-2 text-[24px] transition-all duration-100 ease-in hover:shadow-[0px_0px_10px_rgba(12,241,153,1)]">
+                                                    <a href="https://www.facebook.com/shahzahan.siraj.92"><FaFacebookF className='text-[24px]' /></a>
+                                                  </li>
+                                                  <li className="relative border-2 rounded-[50%] p-2 text-[24px] transition-all duration-100 ease-in hover:shadow-[0px_0px_10px_rgba(12,241,153,1)]">
+                                                    <a href="https://github.com/shahzahandev"><FaGithub className='' /></a>
+                                                  </li>
+                                                    <li className="relative border-2 rounded-[50%] p-2 text-[24px] transition-all duration-100 ease-in hover:shadow-[0px_0px_10px_rgba(12,241,153,1)]">
+                                                    <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl"><BiLogoGmail  className='' /></a>
+                                                  </li>
+                                                </ul>
+                                                     
+                                              </div>
                             </div>
                         </div>
                     </div>
